0df0608ad223017f83ab413d05438daa726ec2e2
imprimeriberac <88195101+imprimeriberac@users.noreply.github.com>
2021-08-20 14:22:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function ChangeClassMenu()  
+{  
+    var scrollY;  
+    if (document.all)  
+    {  
+        if (!document.documentElement.scrollTop)  
+            scrollY = document.body.scrollTop;  
+        else  
+            scrollY = document.documentElement.scrollTop;  
+    }  
+    else
+        scrollY = window.pageYOffset;  
+    if(scrollY > 420)
+        document.getElementById("menu_principal").className = "menu_principal2";  
+    else
+        document.getElementById("menu_principal").className = "menu_principal1";  
+}  
+window.onscroll = ChangeClassMenu;
